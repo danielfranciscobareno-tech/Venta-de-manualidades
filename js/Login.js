99dc7332041aaf8fac5f4ca6506a216c77fc41f5
julian parra <alejoparra1207@gmail.com>
2025-11-01 16:38:43 -0500

    h

--- js/Login.js
+++ js/Login.js
@@ -2,7 +2,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
 import { 
-  collection, query, where, getDocs, getFirestore, getElementById, trim, preventDefault, addEventListener
+  collection, query, where, getDocs, getFirestore, querySelector, getElementById, trim, preventDefault, addEventListener
 } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 
   // TODO: Add SDKs for Firebase products that you want to use
