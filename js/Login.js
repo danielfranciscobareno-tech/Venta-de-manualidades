c8eccc6d8fa85c78f80ab9f8e6a3eddc30b427bb
julian parra <alejoparra1207@gmail.com>
2025-11-01 14:57:18 -0500

    2

--- js/Login.js
+++ js/Login.js
@@ -1,5 +1,4 @@
-
-// registro.js
+// Login.js
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
--- js/registro.js
+++ js/registro.js
@@ -1,16 +1,18 @@
-// registro.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+// Login.js
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// 🔥 Configuración de Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyA6i5fOo7_g_GyvVDZqA3KdI0wL46LSBmw",
-  authDomain: "studio-6064256749-30e66.firebaseapp.com",
-  projectId: "studio-6064256749-30e66",
-  storageBucket: "studio-6064256749-30e66.firebasestorage.app",
-  messagingSenderId: "196390592287",
-  appId: "1:196390592287:web:302538191937b337735a2a"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA6i5fOo7_g_GyvVDZqA3KdI0wL46LSBmw",
+    authDomain: "studio-6064256749-30e66.firebaseapp.com",
+    projectId: "studio-6064256749-30e66",
+    storageBucket: "studio-6064256749-30e66.firebasestorage.app",
+    messagingSenderId: "196390592287",
+    appId: "1:196390592287:web:302538191937b337735a2a"
+  };
 
 // 🔧 Inicializa Firebase y Firestore
 const app = initializeApp(firebaseConfig);
